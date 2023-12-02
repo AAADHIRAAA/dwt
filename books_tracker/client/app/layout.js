a51aab3d2a65870dfd8501bc7247e48de6692382
AAADHIRAAA <padmapriyaindia2004@gmail.com>
2023-12-02 12:13:56 +0530

    updated

--- books_tracker/client/app/layout.js
+++ books_tracker/client/app/layout.js
@@ -12,8 +12,11 @@ export default function RootLayout({ children }) {
   return (
     <ClerkProvider  >
     <html lang="en">
-    
+ 
+    <meta name="viewport" content="width=device-width, initial-scale=1"/>
 
+    
+    
       <body className={inter.className}>
       
         {children}
